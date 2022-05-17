fde7243d1451a6e8ab24ee6f9b231e0b7f7f3669
Sasivardhan11 <98810972+Sasivardhan11@users.noreply.github.com>
2022-05-17 20:26:24 +0530

    n=int(input()) for i in range(n):     for j in reversed(range(1,n+1)):         print(j,end=' ')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,5 @@
 n=int(input())
-for i in range(n,0,-1):
-    for j in range(1,i+1):
-        print(j,end='')
+for i in range(n):
+    for j in reversed(range(1,n+1)):
+        print(j,end=' ')
     print()
