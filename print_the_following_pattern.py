c755ae55e877663fd89d4f4cdcdb71d76a0859a0
Sasivardhan11 <98810972+Sasivardhan11@users.noreply.github.com>
2022-11-06 09:27:57 +0530

    a='ABCDEFGHIJKLMNOPQRSTUVWXYZ' n=int(input()) for i in range(n,0,-1):     for j in range(i):         print(a[i-1],end=' ')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,6 +1,6 @@
+a='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 n=int(input())
-s='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-for i in range(n):
-    for j in range(n):
-        print(s[i],end=' ')
+for i in range(n,0,-1):
+    for j in range(i):
+        print(a[i-1],end=' ')
     print()
